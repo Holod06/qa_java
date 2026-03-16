99c7e92f2f1212d983db1eebb4f1772d2222b2fb
Ilya Kholodilov <kholodilov0601@gmail.com>
2026-03-17 00:41:11 +0300

    second

--- src/test/java/com/exapleTest/FelineTest.java
+++ src/test/java/com/exapleTest/FelineTest.java
@@ -1,35 +1,38 @@
 package com.exapleTest;
-
 import com.example.Feline;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
-
+import org.mockito.Mockito;
+import org.mockito.Spy;
+import org.mockito.junit.jupiter.MockitoExtension;
 import java.util.List;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+
+
+
+
+//Animal наследование , поэтому mock е нужен
+@ExtendWith(MockitoExtension.class)
 public class FelineTest {
 
+    @Spy
+    Feline felineSpy;
+
     @Test
-    void eatMeatShouldReturnPredatorFood() throws Exception {
-
+    void eatMeatShouldReturnFood() throws Exception {
         Feline feline = new Feline();
-
         List<String> expectedFood = List.of("Животные", "Птицы", "Рыба");
-
         List<String> actualFood = feline.eatMeat();
-
         assertEquals(expectedFood, actualFood);
     }
 
     @Test
-    void getFamilyShouldReturnFelineFamily() {
-
+    void getFamilyShouldReturnFamily() {
         Feline feline = new Feline();
-
         String actualFamily = feline.getFamily();
-
         assertEquals("Кошачьи", actualFamily);
     }
 
@@ -46,5 +49,14 @@ public class FelineTest {
         Feline feline = new Feline();
         int result = feline.getKittens(kittensCount);
         assertEquals(kittensCount, result);
+    }
+
+    @Test
+    void eatMeatShouldCallGetFoodMethod() throws Exception {
+        List<String> expectedFood = List.of("Животные", "Птицы", "Рыба");
+        Mockito.doReturn(expectedFood).when(felineSpy).getFood("Хищник");
+        List<String> result = felineSpy.eatMeat();
+        assertEquals(expectedFood, result);
+        Mockito.verify(felineSpy).getFood("Хищник");
     }
 }
